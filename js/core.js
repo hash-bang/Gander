085316072330981c5b9f37bf91f07bdb46d8eac2
Matt Carter <m@ttcarter.com>
2012-07-14 17:40:39 +1000

    BUGFIX: Fixed silly bug where the cached image would be populated with the thumbnail if the thumbnail is being freshly generated

--- js/core.js
+++ js/core.js
@@ -578,7 +578,7 @@ $(function() {
 						if (data.couldthumb)
 							couldthumb++;
 						var fakeicon = (data.realthumb) ? 1:0;
-						var newchild = $('<li rel="' + file + '"><div><div class="imgframe"><img rel="' + fakeicon + '"/></div></div><strong>' + data.title + '</strong><div class="emblems"></div></li>');
+						var newchild = $('<li rel="' + file + '"><div><div class="imgframe"><img class="thumb" rel="' + fakeicon + '"/></div></div><strong>' + data.title + '</strong><div class="emblems"></div></li>');
 						newchild
 							.data({
 								size: data.size,
@@ -586,7 +586,7 @@ $(function() {
 								type: data.type,
 							})
 							.addClass(data.type == 'dir' ? 'folder' : (data.type == 'image' ? 'image' : 'other'))
-							.find('img')
+							.find('img.thumb')
 								.load(function() { $(this).hide(); $.gander.thumbzoom('apply', this); $(this).fadeIn(); $(this).parent('li').css('background', ''); })
 								.attr('src', data.thumb);
 						list.append(newchild);
